f226b7f89ab9422ff167025c7dd4bb47ac5495a9
Christoffer Artmann <Artgaard@gmail.com>
2025-09-11 15:44:22 +0200

    Update api-client.tsx

--- src/app/api-client.tsx
+++ src/app/api-client.tsx
@@ -8,7 +8,6 @@ import {
 } from 'react'
 import { useHotkeys } from 'react-hotkeys-hook'
 
-
 import { Button } from './components/ui/button'
 import { Input } from './components/ui/input'
 import {
