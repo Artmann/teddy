1e7fc2545574001212c65844e0502cb637170e40
Christoffer Artmann <Artgaard@gmail.com>
2025-09-11 15:46:09 +0200

    Add import statement for Button component

--- src/app/api-client.tsx
+++ src/app/api-client.tsx
@@ -8,6 +8,7 @@ import {
 } from 'react'
 import { useHotkeys } from 'react-hotkeys-hook'
 
+
 import { Button } from './components/ui/button'
 import { Input } from './components/ui/input'
 import {
